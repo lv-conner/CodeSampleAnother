d311f08960dd80c009860e7f0fccd8df4f38c7ee
lv-conner <lvmh123456@gmail.com>
2019-08-28 15:48:19 +0800

    embeded file provider

---
+++ FileLoad/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders.Embedded;
+using Microsoft.Extensions.FileProviders;
+using System.Reflection;
+using System.IO;
+
+namespace FileLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            var assembly = Assembly.GetEntryAssembly();
+            services.AddSingleton<IFileProvider>(new EmbeddedFileProvider(assembly));
+
+            var provider = services.BuildServiceProvider();
+            var fileProvider = provider.GetService<IFileProvider>();
+            var file = fileProvider.GetFileInfo("file.hello.txt");
+            if(file == null)
+            {
+                throw new FileNotFoundException();
+            }
+            using(var stream = file.CreateReadStream())
+            {
+                var anotherStream = file.CreateReadStream();
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestCase1();
+            Console.WriteLine("Hello World!");
+        }
+
+        static void TestCase1()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddHttpClient("dishplan", options =>
+             {
+                 options.BaseAddress = new Uri("http://10.1.2.157:13146/api/dishplan/current");
+             }).AddHttpMessageHandler<MyHttpHandler>();
+            services.AddTransient<MyHttpHandler>();
+            var provider = services.BuildServiceProvider();
+
+        }
+    }
+
+    public class MyHttpHandler:DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.TryAddWithoutValidation("content-type", "application/json");
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
