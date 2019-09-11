be26c02c3f1fdf53fc5ac4a5dbd534d23b3d835a
lv-conner <lvmh123456@gmail.com>
2019-09-11 08:49:55 +0800

    lin

---
+++ SystemJsonConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.Json;
+
+namespace SystemJsonConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "tim lv"
+            };
+            var s = JsonSerializer.Serialize(p);
+            var p1 = JsonSerializer.Deserialize<Person>(s);
+            Console.WriteLine(p1.Name);
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
